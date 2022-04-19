c5cdc8f7c4ca1a6bedfaa03d6938edc7d655a7c1
sandipmaharjan95 <sandipmaharjan95@gmail.com>
2022-04-19 20:34:50 +0545

    third

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/about/About';
 import Info from './components/Info/Info'
 import ProductList from './components/productlist/ProductList';
+import Contact from './components/contact/Contact';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <Info/>
       <About/>
       <ProductList/>
+      <Contact/>
       </>
     </div>
   );
